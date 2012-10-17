faf67072553846cb5871b78e0575b208492d5f34
Gabriël Konat <gabrielkonat@gmail.com>
2012-10-17 21:26:09 +0200

    Added nuspec.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gohla.Shared")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Shared classes from Gohla.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gohla.Shared")]
