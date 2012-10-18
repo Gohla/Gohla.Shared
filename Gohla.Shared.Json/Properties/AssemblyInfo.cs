5eef5e0e47c4ca0b9be97a1dc5dc8b901a977d1b
Gabriël Konat <gabrielkonat@gmail.com>
2012-10-18 20:36:15 +0200

    Updated assembly info and makenupkg.

--- Gohla.Shared.Json/Properties/AssemblyInfo.cs
+++ Gohla.Shared.Json/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gohla.Shared.Json")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Shared classes from Gohla")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gohla.Shared.Json")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Gabriël Konat 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
--- makenupkg.bat
+++ makenupkg.bat
@@ -1,3 +1,19 @@
 cd Gohla.Shared
 nuget pack
 move Gohla.Shared.*.nupkg C:\Development\LocalNuget\
+cd..
+
+cd Gohla.Shared.Composition
+nuget pack
+move Gohla.Shared.Composition.*.nupkg C:\Development\LocalNuget\
+cd..
+
+cd Gohla.Shared.Json
+nuget pack
+move Gohla.Shared.Json.*.nupkg C:\Development\LocalNuget\
+cd..
+
+cd Gohla.Shared.XML
+nuget pack
+move Gohla.Shared.XML.*.nupkg C:\Development\LocalNuget\
+cd..
